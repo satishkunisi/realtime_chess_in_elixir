0a4821e03d1154951cd983c11523b1900e1389bb
Satish Kunisi <kunisi@gmail.com>
2018-03-29 21:26:39 -0700

    Add pawn piece module

--- lib/game/piece.ex
+++ lib/game/piece.ex
@@ -1,5 +1,2 @@
 defmodule RealtimeChess.Game.Piece do 
-  @bounds %{row_min: 0, col_min: 0, row_max: 7, col_max: 7} 
-
-  @spec move_positions(tuple) :: MapSet.t
 end
---
+++ test/game/pawn_test.exs
@@ -0,0 +1,16 @@
+defmodule RealtimeChess.Game.PawnTest do
+  use ExUnit.Case, async: true
+  alias RealtimeChess.Game.Pawn
+
+  describe "Pawn.move_positions/1" do
+    test "returns a set with the one position a pawn can move from any given starting position" do
+      assert MapSet.new([{4, 3}]) == Pawn.move_positions({3, 3}, :white)
+      assert MapSet.new([{4, 2}]) == Pawn.move_positions({5, 2}, :black)
+    end
+
+    test "returns a set with the two positions a pawn can move from the starting row" do
+      assert MapSet.new([{3, 3}, {2, 3}]) == Pawn.move_positions({1, 3}, :white)
+      assert MapSet.new([{4, 7}, {5, 7}]) == Pawn.move_positions({6, 7}, :black)
+    end
+  end
+end
