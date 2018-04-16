d1932b83ba9292f2884fd8a07f64f2c5c3b23f95
Satish Kunisi <satish@carbonfive.com>
2018-04-16 09:18:20 -0700

    Add king tests

---
+++ test/game/king_test.exs
@@ -0,0 +1,17 @@
+defmodule RealtimeChess.Game.KingTest do
+  use ExUnit.Case, async: true
+  alias RealtimeChess.Game.King
+
+  describe "King.move_positions/1" do
+    test "returns a set with the eight positions a king can move from any given starting position" do
+      assert MapSet.new([{4, 3}, {2, 3}, {3, 4}, {3, 2}, {4, 4}, {2, 2}, {4, 2}, {2, 2}]) == King.move_positions({3 ,3}, :white)
+      assert MapSet.new([{4, 3}, {2, 3}, {3, 4}, {3, 2}, {4, 4}, {2, 2}, {4, 2}, {2, 2}]) == King.move_positions({3 ,3}, :black)
+    end
+
+
+    test "returns a set with the eight positions a king can attack from any given starting position" do
+      assert MapSet.new([{4, 3}, {2, 3}, {3, 4}, {3, 2}, {4, 4}, {2, 2}, {4, 2}, {2, 2}]) == King.attack_positions({3 ,3}, :white)
+      assert MapSet.new([{4, 3}, {2, 3}, {3, 4}, {3, 2}, {4, 4}, {2, 2}, {4, 2}, {2, 2}]) == King.attack_positions({3 ,3}, :black)
+    end
+  end
+end
