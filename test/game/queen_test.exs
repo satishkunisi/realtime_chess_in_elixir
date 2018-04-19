e3e92e7ada47f840bd2d9fca5cc4463cff158c0a
Satish Kunisi <satish@carbonfive.com>
2018-04-19 08:45:22 -0700

    Add queen test

---
+++ test/game/queen_test.exs
@@ -0,0 +1,10 @@
+defmodule RealtimeChess.Game.QueenTest do
+  use ExUnit.Case, async: true
+  alias RealtimeChess.Game.Queen
+
+  describe "Queen.move_positions/2" do
+    test "returns a set with the positions a queen can move" do
+      {5, 6}, {5, 4}, {6, 5}, {4, 5}, {6, 6}, {4, 4}
+    end
+  end
+end
