f5802e032e5b215faa3d78fe816e8d739f12a74f
Satish Kunisi <kunisi@gmail.com>
2018-10-05 15:38:51 -0700

    Update Elixir and Erlang versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RealtimeChess.Mixfile do
     [
       app: :realtime_chess,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7.3",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
